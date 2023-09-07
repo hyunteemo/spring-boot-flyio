d15166d603e601380510eeb9224244174b88c3b3
lucia.e <lucia.e@kakaoenterprise.com>
2023-09-07 17:44:29 +0900

    hello hyun

--- src/main/java/io/hyun/flyio/springboot/SpringbootApplication.java
+++ src/main/java/io/hyun/flyio/springboot/SpringbootApplication.java
@@ -2,12 +2,18 @@ package io.hyun.flyio.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String HelloHyun() {
+		return "Hello Hyun";
 	}
 
 }
